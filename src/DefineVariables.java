450a0526b664b54e022e5565e873ec0ca51bdf26
Lucy Hu <lucyhu@LucydeiMac.local>
2021-03-07 00:50:30 -0500

    javaHomewrok1

---
+++ src/DefineVariables.java
@@ -0,0 +1,26 @@
+
+public class DefineVariables {
+	public static void main(String[] args) {
+		String bunny = "a";
+		int age;
+		Integer streetNumber = Integer.valueOf(99);
+		Boolean turnLeft = Boolean.valueOf(false);
+		Boolean gostraght = Boolean.valueOf(bunny);
+
+		DefineVariables v = new DefineVariables();
+		
+		System.out.println(bunny);
+		System.out.println(false);
+		System.out.println(false);
+		System.out.println(age);
+		String jumping_bunny="b";
+		String Jumping_bunny="c";
+		String hu34="lucy";
+		Integer amount$=5;
+		
+		int birthday=Integer.valueOf(0);
+		
+		
+
+	}
+}
