2a24d04451455fa060b4f892df9abe2635c1b85d
xinxinlucyhu <64392681+xinxinlucyhu@users.noreply.github.com>
2021-03-11 01:52:18 -0500

    Add files via upload

--- src/DefineVariables.java
+++ src/DefineVariables.java
@@ -2,7 +2,7 @@
 public class DefineVariables {
 	public static void main(String[] args) {
 		String bunny = "a";
-		int age;
+		int age=0;
 		Integer streetNumber = Integer.valueOf(99);
 		Boolean turnLeft = Boolean.valueOf(false);
 		Boolean gostraght = Boolean.valueOf(bunny);
