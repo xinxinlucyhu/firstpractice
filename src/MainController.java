8363b5272d23bee6d66dca6ef66e93ccdb27cb29
xinxinlucyhu <64392681+xinxinlucyhu@users.noreply.github.com>
2021-03-22 23:38:59 -0400

    Add files via upload

---
+++ src/MainController.java
@@ -0,0 +1,15 @@
+package com.tts.SpringBootHomeworkProject.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController
+{
+    @RequestMapping("/")
+    public String home()
+    {
+        return "Hello World!";
+
+    }
+}
